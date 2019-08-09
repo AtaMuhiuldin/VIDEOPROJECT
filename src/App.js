af6c7061b1c552480101db8c2e7f5e25c5b38335
muhammad salman <muhammadsalman00012@gmail.com>
2019-08-09 11:14:08 +0500

    changes done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render(){
-  return (
-    <div className="App">
-        <h1>Hellow </h1>
-    </div>
-  );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1>Hellow </h1>
+				<h1>Salman </h1>
+			</div>
+		);
+	}
 }
 
 export default App;
